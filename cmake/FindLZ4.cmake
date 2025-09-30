7962aa0578bae36cb86af978620b3c9549291cc3
= <danworley@gmail.com>
2025-09-29 20:23:05 -0400

    uwp: checkpoint early build restructure
    Setting up clang static lib build to be consumed by future UWP app.
    Temporarily disabling SDL3 (saves a tiny bit of memory)
    Fix vulkan import issues

--- cmake/FindLZ4.cmake
+++ cmake/FindLZ4.cmake
@@ -20,26 +20,30 @@
 # LZ4_LIBRARY
 #
 
-find_path(LZ4_INCLUDE_DIR NAMES lz4.h)
+if (NOT UWP_BUILD)
+	find_path(LZ4_INCLUDE_DIR NAMES lz4.h)
 
-find_library(LZ4_LIBRARY_DEBUG NAMES lz4d)
-find_library(LZ4_LIBRARY_RELEASE NAMES lz4)
+	find_library(LZ4_LIBRARY_DEBUG NAMES lz4d)
+	find_library(LZ4_LIBRARY_RELEASE NAMES lz4)
 
-include(SelectLibraryConfigurations)
-SELECT_LIBRARY_CONFIGURATIONS(LZ4)
+	include(SelectLibraryConfigurations)
+	SELECT_LIBRARY_CONFIGURATIONS(LZ4)
 
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-    LZ4 DEFAULT_MSG
-    LZ4_LIBRARY LZ4_INCLUDE_DIR
-)
+	include(FindPackageHandleStandardArgs)
+	FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+	    LZ4 DEFAULT_MSG
+	    LZ4_LIBRARY LZ4_INCLUDE_DIR
+	)
 
-mark_as_advanced(LZ4_INCLUDE_DIR LZ4_LIBRARY)
+	mark_as_advanced(LZ4_INCLUDE_DIR LZ4_LIBRARY)
 
-if(LZ4_FOUND AND NOT (TARGET LZ4::LZ4))
-  add_library (LZ4::LZ4 UNKNOWN IMPORTED)
-  set_target_properties(LZ4::LZ4
-    PROPERTIES
-    IMPORTED_LOCATION ${LZ4_LIBRARY}
-    INTERFACE_INCLUDE_DIRECTORIES ${LZ4_INCLUDE_DIR})
+	if(LZ4_FOUND AND NOT (TARGET LZ4::LZ4))
+	  add_library (LZ4::LZ4 UNKNOWN IMPORTED)
+	  set_target_properties(LZ4::LZ4
+	    PROPERTIES
+	    IMPORTED_LOCATION ${LZ4_LIBRARY}
+	    INTERFACE_INCLUDE_DIRECTORIES ${LZ4_INCLUDE_DIR})
+	endif()
+else()
+	find_package(lz4 CONFIG REQUIRED)
 endif()
