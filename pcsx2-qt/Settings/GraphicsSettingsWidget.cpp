PCSX2/pcsx2

Split up HDR into 4 different presets (including "Off", which is identical to the old default behaviour). Proper HDR is currently only available in DX11/12, but these have barrier problems with SW blends and thus produce many random glitches. Press "L" to swap between the HDR modes (temporary code). HDR is currently broken anyway, there's probably some dumb mistake in one of the last changes, no time to check now.

pcsx2-qt/Settings/GraphicsSettingsWidget.cpp
@@ -165,7 +165,7 @@ GraphicsSettingsWidget::GraphicsSettingsWidget(SettingsWindow* dialog, QWidget* 
 		s_anisotropic_filtering_entries, s_anisotropic_filtering_values, "0");
 	SettingWidgetBinder::BindWidgetToIntSetting(sif, m_ui.dithering, "EmuCore/GS", "dithering_ps2", 2);
 	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.mipmapping, "EmuCore/GS", "hw_mipmap", true);
-	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.hdr, "EmuCore/GS", "hdr", false); //TODO: expose to SW renderer too? Or split the HDR textures vs HDR output
+	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.hdr, "EmuCore/GS", "hdr", false); //TODO1: expose to SW renderer too? Or split the HDR textures vs HDR output. Or anyway add settings for all 4 enum HDR options?
 	SettingWidgetBinder::BindWidgetToIntSetting(
 		sif, m_ui.blending, "EmuCore/GS", "accurate_blending_unit", static_cast<int>(AccBlendLevel::Basic));
 	SettingWidgetBinder::BindWidgetToIntSetting(

worleydl/pcsx2-dev

pcsx2-qt/Settings/GraphicsSettingsWidget.cpp
@@ -111,7 +111,7 @@ GraphicsSettingsWidget::GraphicsSettingsWidget(SettingsWindow* settings_dialog, 
 		s_anisotropic_filtering_entries, s_anisotropic_filtering_values, "0");
 	SettingWidgetBinder::BindWidgetToIntSetting(sif, m_hw.dithering, "EmuCore/GS", "dithering_ps2", 2);
 	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_hw.mipmapping, "EmuCore/GS", "hw_mipmap", true);
-	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_hw.hdr, "EmuCore/GS", "hdr", false); //TODO: expose to SW renderer too? Or split the HDR textures vs HDR output
+	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_hw.hdr, "EmuCore/GS", "hdr", false); //TODO1: expose to SW renderer too? Or split the HDR textures vs HDR output. Or anyway add settings for all 4 enum HDR options?
 	SettingWidgetBinder::BindWidgetToIntSetting(
 		sif, m_hw.blending, "EmuCore/GS", "accurate_blending_unit", static_cast<int>(AccBlendLevel::Basic));
 	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_hw.enableHWFixes, "EmuCore/GS", "UserHacks", false);